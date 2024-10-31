960b63abc4706a2acfd60efd68cc4ed239861c0b
pxwxnvermx <pawanjbs5@gmail.com>
2024-10-31 11:53:47 +0530

    Add X, O cells and chances

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,30 @@ const r = @cImport(@cInclude("raylib.h"));
 const WIDTH = 900;
 const GRID_SIZE = 3;
 const CELL_SIZE = WIDTH / 3 - 2;
+const CELLS = enum {
+    E,
+    O,
+    X,
+    pub fn str(self: CELLS) [:0]const u8 {
+        return switch (self) {
+            CELLS.X => "X",
+            CELLS.O => "O",
+            CELLS.E => " ",
+        };
+    }
+};
+const PLAYER_CELL = CELLS.O;
 
 pub fn main() !void {
     r.SetConfigFlags(r.FLAG_VSYNC_HINT);
     r.InitWindow(WIDTH, WIDTH, "Zig Zac Zoe");
     defer r.CloseWindow();
+
+    var board = [_][3]CELLS{
+        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
+        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
+        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
+    };
 
     while (!r.WindowShouldClose()) {
         r.BeginDrawing();
@@ -19,8 +38,24 @@ pub fn main() !void {
             for (0..GRID_SIZE) |_col| {
                 const col = @as(c_int, @intCast(_col));
                 const y = col * CELL_SIZE;
+                const cell = r.Rectangle{
+                    .x = @floatFromInt(x + 2),
+                    .y = @floatFromInt(y + 2),
+                    .width = CELL_SIZE,
+                    .height = CELL_SIZE,
+                };
                 r.DrawRectangle(x, y, CELL_SIZE + 8, CELL_SIZE + 8, r.WHITE);
                 r.DrawRectangle(x + 2, y + 2, CELL_SIZE, CELL_SIZE, r.BLACK);
+                r.DrawRectangleRec(cell, r.BLACK);
+                const sym = board[_row][_col].str();
+                r.DrawText(sym, x + 65, y + 20, CELL_SIZE - 20, r.WHITE);
+                if (r.CheckCollisionPointRec(r.GetMousePosition(), cell) and !cpu_turn) {
+                    if (r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT)) {
+                        board[_row][_col] = PLAYER_CELL;
+                        cpu_turn = true;
+                    }
+                    r.DrawText(PLAYER_CELL.str(), x + 65, y + 20, CELL_SIZE - 20, r.GRAY);
+                }
             }
         }
 
