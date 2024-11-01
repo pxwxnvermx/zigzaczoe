8edb4a1812b3b222db147e8e455d58351cefc97f
pxwxnvermx <pawanjbs5@gmail.com>
2024-11-01 10:48:06 +0530

    Refactor simplify cell logic

--- src/main.zig
+++ src/main.zig
@@ -1,31 +1,22 @@
+const std = @import("std");
 const r = @cImport(@cInclude("raylib.h"));
+
+const str = *const [1:0]u8;
 
 const WIDTH = 900;
 const GRID_SIZE = 3;
 const CELL_SIZE = WIDTH / 3 - 2;
-const CELLS = enum {
-    E,
-    O,
-    X,
-    pub fn str(self: CELLS) [:0]const u8 {
-        return switch (self) {
-            CELLS.X => "X",
-            CELLS.O => "O",
-            CELLS.E => " ",
-        };
-    }
-};
-const PLAYER_CELL = CELLS.O;
+const PLAYER_CELL = "O";
+const CPU_CELL = "X";
 
 pub fn main() !void {
     r.SetConfigFlags(r.FLAG_VSYNC_HINT);
     r.InitWindow(WIDTH, WIDTH, "Zig Zac Zoe");
     defer r.CloseWindow();
-
-    var board = [_][3]CELLS{
-        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
-        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
-        [_]CELLS{ CELLS.E, CELLS.E, CELLS.E },
+    var board = [3][3]str{
+        [3]str{ " ", " ", " " },
+        [3]str{ " ", " ", " " },
+        [3]str{ " ", " ", " " },
     };
 
     var cpu_turn = false;
@@ -48,18 +39,18 @@ pub fn main() !void {
                 };
                 r.DrawRectangle(x, y, CELL_SIZE + 8, CELL_SIZE + 8, r.WHITE);
                 r.DrawRectangleRec(cell, r.BLACK);
-                const sym = board[_row][_col].str();
+                const sym = board[_row][_col];
                 r.DrawText(sym, x + 65, y + 20, CELL_SIZE - 20, r.WHITE);
-                if (cpu_turn and board[_row][_col] == CELLS.E) {
-                    board[_row][_col] = CELLS.X;
+                if (cpu_turn and std.mem.eql(u8, sym, " ")) {
+                    board[_row][_col] = CPU_CELL;
                     cpu_turn = false;
                 }
-                if (r.CheckCollisionPointRec(r.GetMousePosition(), cell) and !cpu_turn) {
+                if (r.CheckCollisionPointRec(r.GetMousePosition(), cell) and !cpu_turn and std.mem.eql(u8, sym, " ")) {
                     if (r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT)) {
                         board[_row][_col] = PLAYER_CELL;
                         cpu_turn = true;
                     }
-                    r.DrawText(PLAYER_CELL.str(), x + 65, y + 20, CELL_SIZE - 20, r.GRAY);
+                    r.DrawText(PLAYER_CELL, x + 65, y + 20, CELL_SIZE - 20, r.GRAY);
                 }
             }
         }
