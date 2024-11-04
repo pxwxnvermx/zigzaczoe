8937387027da0702fd03c3ca4a75fbcf4a3ec10f
Pawan Verma <pverma@dimagi.com>
2024-11-04 16:44:30 +0530

    Add better random picking for cpu turn

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,12 @@ const CPU_CELL = "X";
 
 pub fn main() !void {
     r.SetConfigFlags(r.FLAG_VSYNC_HINT);
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
     r.InitWindow(WIDTH, WIDTH, "Zig Zac Zoe");
     defer r.CloseWindow();
     var board = [3][3]str{
@@ -24,6 +30,17 @@ pub fn main() !void {
     while (!r.WindowShouldClose()) {
         r.BeginDrawing();
         r.ClearBackground(r.RAYWHITE);
+
+        var random_x = rand.intRangeAtMost(usize, 0, 2);
+        var random_y = rand.intRangeAtMost(usize, 0, 2);
+        while (!std.mem.eql(u8, board[random_x][random_y], " ")) {
+            random_x = rand.intRangeAtMost(usize, 0, 2);
+            random_y = rand.intRangeAtMost(usize, 0, 2);
+        }
+        while (std.mem.eql(u8, board[random_x][random_y], " ") and cpu_turn) {
+            board[random_x][random_y] = CPU_CELL;
+            cpu_turn = false;
+        }
 
         for (0..GRID_SIZE) |_row| {
             const row = @as(c_int, @intCast(_row));
@@ -41,10 +58,6 @@ pub fn main() !void {
                 r.DrawRectangleRec(cell_rect, r.BLACK);
                 const cell = board[_row][_col];
                 r.DrawText(cell, x + 65, y + 20, CELL_SIZE - 20, r.WHITE);
-                if (cpu_turn and std.mem.eql(u8, cell, " ")) {
-                    board[_row][_col] = CPU_CELL;
-                    cpu_turn = false;
-                }
                 const cell_is_clicked = r.CheckCollisionPointRec(r.GetMousePosition(), cell_rect);
                 if (cell_is_clicked and !cpu_turn and std.mem.eql(u8, cell, " ")) {
                     if (r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT)) {
